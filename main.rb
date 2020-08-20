73aa3d7dfec5952682ef38d3eeca37c320c6f262
James Yeates <jimibue@gmail.com>
2020-08-20 11:30:03 -0700

    divide working

--- main.rb
+++ main.rb
@@ -1,10 +1,16 @@
 def add(num1, num2)
   num1 + num2
 end
+
 # this minus works
 def minus(num1, num2)
   num1 - num2
 end
 
+def divide(num1, num2)
+  num1 / num2
+end
+
 puts add(1, 1)
 puts minus(1, 1)
+puts divide(1, 1)
